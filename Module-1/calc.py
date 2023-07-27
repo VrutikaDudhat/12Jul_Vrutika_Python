05b71c1317827298f464da9c028fad660881b50d
VrutikaDudhat <vrutikadudhat3@gmail.com>
2023-07-27 22:07:41 +0530

    Module

---
+++ Module-1/calc.py
@@ -0,0 +1,7 @@
+a=25
+b=50
+
+print("sum:",a+b)
+print("sub:",a-b)
+print("mul:",a*b)
+print("div:",a/b)
---
+++ Module-1/format.py
@@ -0,0 +1,12 @@
+#id=1
+#name=23 # not use the string value
+
+
+#print("My ID is {} and Name is {}".format(id,name))
+#print(f"My ID {id} and Name is {name}")
+
+
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+
+print(f"A={a}, B={b} and sum of {a}+{b}={a+b}")
